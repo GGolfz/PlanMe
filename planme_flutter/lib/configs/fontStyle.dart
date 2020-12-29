33039a6e12d55ea67f3b30c68d75ade75c5f94d3
GGolfz <wisarutgolf11@gmail.com>
2020-12-29 21:29:53 +0700

    [ADD] Font Style

---
+++ planme_flutter/lib/configs/fontStyle.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const titleText =
+    TextStyle(fontFamily: 'Nutino', fontSize: 22, fontWeight: FontWeight.bold);
+const subTitleText =
+    TextStyle(fontFamily: 'Nutino', fontSize: 20, fontWeight: FontWeight.w600);
+const normalText =
+    TextStyle(fontFamily: 'Nutino', fontSize: 18, fontWeight: FontWeight.w400);
+const buttonText =
+    TextStyle(fontFamily: 'Nutino', fontSize: 18, fontWeight: FontWeight.w600);
+const lightText =
+    TextStyle(fontFamily: 'Nutino', fontSize: 16, fontWeight: FontWeight.w300);
