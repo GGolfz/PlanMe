39d689d2d893ac4bc5972c02bb08b0945c6d10d8
GGolfz <wisarutgolf11@gmail.com>
2021-01-05 22:21:34 +0700

    [ADD] Achievement Alert Decorate

--- planme_flutter/lib/widgets/achievement/achievement_alert.dart
+++ planme_flutter/lib/widgets/achievement/achievement_alert.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:planme_flutter/configs/color.dart';
+import 'package:planme_flutter/configs/fontStyle.dart';
 
 class AchievementAlert extends StatelessWidget {
   final title;
@@ -7,7 +9,29 @@ class AchievementAlert extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      content: Text(title),
+      insetPadding: EdgeInsets.all(40),
+      title: Row(
+        children: [Text(title)],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+      content: SizedBox(
+          width: 100,
+          height: 100,
+          child: Image.network(
+            'https://storage.googleapis.com/planme_storage/stickers/$image',
+            fit: BoxFit.contain,
+          )),
+      actions: [
+        RaisedButton(
+            color: successColor,
+            child: Text(
+              "OK",
+              style: subTitleText.copyWith(color: deepWhite),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            })
+      ],
     );
   }
 }
